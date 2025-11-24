5590f362bcc5f09cc37ecd64ccba03196b7f65e5
JusticeIL <galru3@mta.ac.il>
2025-11-24 12:26:40 +0200

    submission

---
+++ mapper.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+"""mapper.py"""
+
+import sys
+
+def read_input(file):
+    for line in file:
+        # split the line into words
+        yield line.split()
+
+def main(separator='\t'):
+    # input comes from STDIN (standard input)
+    data = read_input(sys.stdin)
+    for words in data:
+        # write the results to STDOUT (standard output);
+        # what we output here will be the input for the
+        # Reduce step, i.e. the input for reducer.py
+        #
+        # tab-delimited; the trivial word count is 1
+        for word in words:
+            # strip trailing commas and dots at the end of the words while also counting words as case-insensitive
+            word = word.strip('.,').lower()
+            print ('%s%s%d' % (word, separator, 1))
+
+if __name__ == "__main__":
+    main()
---
+++ reducer.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+"""reducer.py"""
+
+from itertools import groupby
+from operator import itemgetter
+import sys
+
+def read_mapper_output(file, separator='\t'):
+    for line in file:
+        yield line.rstrip().split(separator, 1)
+
+def main(separator='\t'):
+    # input comes from STDIN (standard input)
+    data = read_mapper_output(sys.stdin, separator=separator)
+    # declaring a dictionary to easily hold and manipulate the words data inside
+    words = {}
+    # groupby groups multiple word-count pairs by word,
+    # and creates an iterator that returns consecutive keys and their group:
+    #   current_word - string containing a word (the key)
+    #   group - iterator yielding all [current_word, count] items
+    for current_word, group in groupby(data, itemgetter(0)):
+        try:
+            total_count = sum(int(count) for current_word, count in group)
+            words[current_word] = total_count
+        except ValueError:
+            # count was not a number, so silently discard this item
+            pass
+
+    # printing final 3 highest scores of words
+    top_3_highest_scores = sorted(words.items(), key=lambda w: w[1], reverse=True)[:3]
+    for word, count in top_3_highest_scores:
+        print ("%s%s%d" % (word, separator, count))
+
+if __name__ == "__main__":
+    main()
